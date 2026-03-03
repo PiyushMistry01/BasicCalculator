d2339c7e01ca7759c3ce58c0332a38627fd77fb0
PIYUSH-MISTRY69 <piyushmistry12300@gmail.com>
2026-03-03 14:14:15 +0530

    Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import './App.css'
 function App(){
 
-const[number,setnumber] = useState("");
+const [number,setnumber] = useState("");
 const [operands,setoperands] = useState("");
 const [previousnumber,setpreviousnumber] = useState("");
 const [result,setresult] = useState("")
@@ -18,7 +18,7 @@ setnumber("");
 setresult("");
 }
 
-function handleOprations(){
+function handleOperations(){
   var math = 0
 switch (operands) {
   case "+": math = Number(previousnumber) + Number(number)
@@ -42,23 +42,45 @@ setoperands("");
 setpreviousnumber("");
 }
 
-return <div className="main">
+const buttons = [
+  "1","2","3",
+  "4","5","6",
+  "7","8","9",
+  "+","0","-",
+  "/","=","x"
+];
+
+return (  
+<div className="main">
   <div className="calc">
-<div className="display">
-  {result
-    ? result
-    : `${previousnumber} ${operands} ${number}`.trim() || 0}
+    <div className="display">
+      {result
+        ? result
+        : `${previousnumber} ${operands} ${number}`.trim() || 0}
+    </div>
+    <div className="btn-container">
+      {buttons.map((btn) => (
+        <button
+          key={btn}
+          onClick={() => {
+            if (btn === "=") {
+              handleOperations();
+            } 
+            else if (["+", "-", "X", "/"].includes(btn)) {
+              handleOperands(btn);
+            } 
+            else {
+              handleDigits(btn);
+            }
+          }}
+        >
+          {btn}
+        </button>
+       ))}
+    </div>
+ </div>
 </div>
-<div className="but-container">
-  {[1,2,3,4,5,6,7,8,9,0].map((digits) => (
-<button key={digits} onClick={()=>{handleDigits(digits.toString())}} >{digits}</button>
-  ))}
-  {["+","-","/","X"].map((operands)=>(
-    <button key={operands} onClick={()=>{handleOperands(operands)}}>{operands}</button>
-  ))}
-  <button onClick={handleOprations}> = </button>
-</div>
-</div>
-</div>
+);
 }
+
 export default App
